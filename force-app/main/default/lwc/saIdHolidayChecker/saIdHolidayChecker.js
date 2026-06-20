475d67c8146feae6a190a23e57beee826febfa9c
AnshitaMotiramani <anshitamotiramani13@gmail.com>
2026-06-20 14:57:19 +0530

    Implement user story 2 ID validation

--- force-app/main/default/lwc/saIdHolidayChecker/saIdHolidayChecker.js
+++ force-app/main/default/lwc/saIdHolidayChecker/saIdHolidayChecker.js
@@ -4,12 +4,37 @@ import searchIdNumber from '@salesforce/apex/SAIdHolidayController.searchIdNumbe
 export default class SaIdHolidayChecker extends LightningElement {
     idNumber = '';
     message = '';
+    errorMessage = '';
+
+    get isSearchDisabled() {
+        return Boolean(this.errorMessage) || !this.idNumber;
+    }
 
     handleIdChange(event) {
         this.idNumber = event.target.value;
+        this.message = '';
+        this.validateIdNumber();
+    }
+
+    validateIdNumber() {
+        const idPattern = /^[0-9]{13}$/;
+
+        if (!this.idNumber) {
+            this.errorMessage = 'Please enter a South African ID Number.';
+        } else if (!idPattern.test(this.idNumber)) {
+            this.errorMessage = 'ID Number must contain exactly 13 digits.';
+        } else {
+            this.errorMessage = '';
+        }
     }
 
     async handleSearch() {
+        this.validateIdNumber();
+
+        if (this.isSearchDisabled) {
+            return;
+        }
+
         try {
             this.message = await searchIdNumber({
                 idNumber: this.idNumber
